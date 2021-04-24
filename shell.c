acb8ae6b86f8b5e64c1c737705a0c00d505f8be4
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2021-04-23 21:11:00 -0300

    Mudamos umas coisas ae

--- shell.c
+++ shell.c
@@ -8,69 +8,94 @@
 #include <string.h>
 #include "Corzinha.h"
 #include <limits.h>
+#include <sys/wait.h>
 
 #define ARG_MAX 3
 
-void execCommand(char * fullCommand){
+typedef struct COMANDO{
+    char* fullCommand;
+    char* command;
+    char** args;
+    int nargs;
+}tCommand;
 
-    char * save,
-         * command = strtok_r(fullCommand, " ", &save),
-         * arg = strtok_r(NULL, " ", &save),
-         * all_args[ARG_MAX];
+void preencheArgumentos(tCommand* cmd){
+
+    char* save;
+
+    cmd->nargs = 0;
+    cmd->command = strtok_r(cmd->fullCommand, " ", &save);
+
+    char    * arg = strtok_r(NULL, " ", &save),
+            * all_args[ARG_MAX];
 
     // Find all args from command
-    int num_args = 0;
-    for (num_args; arg; num_args++, arg=strtok_r(NULL, " ", &save))
-        all_args[num_args] = arg;
+    for (cmd->nargs; arg; cmd->nargs++, arg=strtok_r(NULL, " ", &save))
+        all_args[cmd->nargs] = arg;
 
     // Create arg vector to execute command.
-    char ** args = malloc(sizeof(*args) * (num_args + 2));
-    args[0] = command;
-    for (int i=1; i < num_args; i++){
-        args[i] = all_args[i-1];
-        printf("args[%d], %s\n", i, args[i]);
-    }
-    args[num_args+1] = NULL;
+    cmd->args = malloc(sizeof(char*) * (cmd->nargs + 2));
+    cmd->args[0] = cmd->command;
 
-    printf("Vetor args:\n");
-    for (int i=0; i < num_args+1; i++){
-        if (i == num_args) printf("\"%s\", ", args[i]);
-        else printf("\"%s\"\n", args[i]);
+    for (int i=1; i <= cmd->nargs; i++){
+        cmd->args[i] = all_args[i-1];
     }
-    printf("\n");
-    // exec()
+
+    cmd->args[cmd->nargs + 1] = NULL;
+
+}
+
+void execCommand(tCommand* cmd){
+
+    int pid = fork();
+    if(pid == 0) {
+        printf("[FILHO]: Vou executar o comando \"%s\"\n", cmd->command);
+        execvp(cmd->command, cmd->args);
+    }
+    else{
+        printf("[PAI]: O pid do filho é %d\n", pid);
+        waitpid(pid, NULL, 0);
+    }
+
+}
+
+void freeComand(tCommand* cmd) {
+    free(cmd->args);
 }
 
 int readLine(){
 
+    tCommand comando[5];
+    int i = 0;
+
     char command_line[LINE_MAX], * save;
+
     if (scanf("%[^\n]%*c", command_line) != 1) return 0;
 
-    printf("COMAND LINE[1]: \"%s\"\n", command_line);
-    char * command = strtok_r(command_line, "|", &save);
-    printf("COMAND LINE[2]: \"%s\"\n", command_line);
+    comando[i].fullCommand = strtok_r(command_line, "|", &save);
 
-    while (command){
-        printf("COMANDO: \"%s\"\n", command);
-        if(strcmp(command, "armageddon") == 0){
-            exit(0);
+    while (comando[i].fullCommand){
+        if(strcmp(comando[i].fullCommand, "armageddon") == 0){
+            return(0);
         }
-//        char * aux = malloc(sizeof(char) * strlen(command));
-//        strcpy(aux, command);
-//        execCommand(aux);
-        execCommand(command);
-        command = strtok_r(NULL, "|", &save);
+        preencheArgumentos(&comando[i]);
+        i++;
+        comando[i].fullCommand = strtok_r(NULL, "|", &save);
     }
+
+    for(int j=0; j<i; j++) {
+        execCommand(&comando[j]);
+        freeComand(&comando[j]);
+    }
+
     return 1;
 
 }
 
-
-
 char * removePath(char * path){
 
     int i;
-    for (i=strlen(path)-1; path[i] != '/'; i--);
+    for (i = strlen(path)-1; path[i] != '/'; i--);
     char * new = malloc(sizeof(char) * (strlen(path)-i));
     for (int j=i+1; j <= strlen(path); j++){
         new[j-(i+1)] = path[j];
@@ -82,9 +107,12 @@ void printLineCommand(){
 
     char cwd[PATH_MAX], user[NAME_MAX];
     getcwd(cwd, sizeof(cwd));
+    char* func = removePath(cwd);
     printf(BOLD MAGENTA "afv:vsh "
            BOLD VERDE "%s "
-           BOLD AZUL "%s > " RESET, getlogin(), removePath(cwd));
+           BOLD AZUL "%s > " RESET, getlogin(), func);
+    free(func);
+
 }
 
 void run_shell() {
@@ -92,4 +120,5 @@ void run_shell() {
     do {
         printLineCommand();
     } while (readLine());
+
 }
